3e2499f4909c99988faf75ea7023e49e5ed84205
Sanjyot <sanjyottopkar@gmail.com>
2026-03-26 12:59:40 +0530

    Added exp-11

--- exp-11/customer_service.py
+++ exp-11/customer_service.py
@@ -32,4 +32,8 @@ def get_orders(customer_id):
     return jsonify({"error": "Customer not found"}), 404
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+     import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
--- exp-11/order_service.py
+++ exp-11/order_service.py
@@ -36,4 +36,9 @@ def update_order(order_id):
     })
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    
+    import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
